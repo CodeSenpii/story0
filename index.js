f80a98be879d96262f307fb747cc62362d29d641
kieyn.parks <kieyn.parks@snhu.edu>
2021-11-17 16:23:28 -0500

    hid nav btn and make visible on cover page button press

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ function setup(){
 
   var read_btn = document.getElementById('btn-read');
   var cover = document.getElementsByClassName('book-cover');
+  var nav_btn = document.getElementsByClassName('nav-btn');
   console.log(cover[0]);
 
   read_btn.addEventListener("click", function (){
 
     cover[0].style.visibility = 'hidden';
+    nav_btn[0].style.visibility = 'visible';
+    nav_btn[1].style.visibility = 'visible';
   });
 
 
